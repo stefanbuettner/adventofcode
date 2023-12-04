60df92d8824d4a7480866143a4e50280d86a8aaf
Stefan Büttner <buettner.stefan@web.de>
2023-12-04 20:10:17 +0100

    First, horrible (maybe) solution to 04.12.2023 Pt. 2

--- 2023/04/solve.py
+++ 2023/04/solve.py
@@ -1,17 +1,45 @@
 import math
+import time
+
+def parse_card_reward_count(line):
+    _, numbers = line.split(":")
+    first, second = numbers.split("|")
+    first_set = set(map(int, filter(lambda s: s != "", first.strip().split(" "))))
+    second_set = set(map(int, filter(lambda s: s != "", second.strip().split(" "))))
+    return len(first_set.intersection(second_set))
+
+def part1(line):
+    count = parse_card_reward_count(line)
+    if count <= 0:
+        return 0
+    return int(math.pow(2, count - 1))
+
+
+# {nr: count, ...}
+card_rewards = {}
+# Card numbers, 1 based
+card_queue = []
 
 with open("input.txt") as f:
     foo = []
-    for line in f:
-        #print(line)
-        card, numbers = line.split(":")
-        first, second = numbers.split("|")
-        first_set = set(first.strip().split(" "))
-        second_set = set(second.strip().split(" "))
-        in_both_sets = first_set.intersection(second_set)
-        if len(in_both_sets) > 0:
-            #print(in_both_sets)
-            foo.append(int(math.pow(2, len(in_both_sets) - 1)))
+    content = [line.strip() for line in f]
+    for line in content:
+        foo.append(part1(line))
 
-    #print(foo)
-    print(sum(foo))
+    print("Part 1: ", sum(foo))
+    
+    card_queue = list(range(1, len(content) + 1))
+    number_cards_won = 0
+    while len(card_queue) > 0:
+        #print(card_queue)
+        number_cards_won += 1
+        card_nr = card_queue.pop(0)
+        if card_nr not in card_rewards:
+            card_reward_count = parse_card_reward_count(content[card_nr - 1])
+            card_rewards[card_nr] = card_reward_count
+        else:
+            card_reward_count = card_rewards[card_nr]
+        card_queue.extend([card_nr + 1 + k for k in range(0, card_reward_count)])
+        #time.sleep(0.5)
+    
+    print("Part 2: ", number_cards_won)
