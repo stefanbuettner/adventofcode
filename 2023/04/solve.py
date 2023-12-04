a84b7febb5c2ed1e5c1f921f76d3ec43d52353ba
Stefan Büttner <buettner.stefan@web.de>
2023-12-04 16:45:37 +0100

    Continue on 04.12.2023

--- 2023/04/solve.py
+++ 2023/04/solve.py
@@ -1,8 +1,17 @@
-with open("example.txt") as f:
+import math
+
+with open("input.txt") as f:
+    foo = []
     for line in f:
+        #print(line)
         card, numbers = line.split(":")
         first, second = numbers.split("|")
-        first_set = set(first.split(" "))
-        second_set = set(second.split(" "))
+        first_set = set(first.strip().split(" "))
+        second_set = set(second.strip().split(" "))
         in_both_sets = first_set.intersection(second_set)
-        print(line)
+        if len(in_both_sets) > 0:
+            #print(in_both_sets)
+            foo.append(int(math.pow(2, len(in_both_sets) - 1)))
+
+    #print(foo)
+    print(sum(foo))
