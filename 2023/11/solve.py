8b73f0a5bb2396c36bd26e129b05bf20126136c8
Stefan Büttner <buettner.stefan@web.de>
2023-12-11 08:56:29 +0100

    Clean up solution 11.12.2023

--- 2023/11/solve.py
+++ 2023/11/solve.py
@@ -20,10 +20,6 @@ def find_galaxy_locations(content):
                 locations.append((x, y))
     return locations
 
-def print_content(content):
-    for line in content:
-        print(line)
-
 def get_expansion_indices(galaxy_map, galaxy_locations):
     width = len(galaxy_map[0])
     height = len(galaxy_map)
@@ -31,26 +27,6 @@ def get_expansion_indices(galaxy_map, galaxy_locations):
     expand_x = set(range(width)).difference(x)
     expand_y = set(range(height)).difference(y)
     return sorted(expand_x), sorted(expand_y)
-
-def correct_for_cosmic_expansion(galaxy_map, galaxy_locations):
-    """
-    Duplicates rows and columns which do not contain galaxies.
-    """
-    expand_x, expand_y = get_expansion_indices(galaxy_map, galaxy_locations)
-    new_map = []
-    for y in range(len(galaxy_map)):
-        row = ""
-        for x in range(len(galaxy_map[y])):
-            row += galaxy_map[y][x]
-            if x in expand_x:
-                row += galaxy_map[y][x]
-        new_map.append(row)
-        if y in expand_y:
-            new_map.append(row)
-    return new_map
-
-def galaxy_distance(a, b):
-    return abs(b[0] - a[0]) + abs(b[1] - a[1])
 
 class GalaxyDistanceFunctor:
     def __init__(self, expansion_multiplier, indices_x : list, indices_y : list):
@@ -68,18 +44,12 @@ class GalaxyDistanceFunctor:
         y2 = max(a[1], b[1])
         insert_count_x = bisect.bisect_left(self.indices_x, x2) - bisect.bisect_left(self.indices_x, x1)
         insert_count_y = bisect.bisect_left(self.indices_y, y2) - bisect.bisect_left(self.indices_y, y1)
-        #print(insert_count_x, insert_count_y)
         return x2 - x1 + y2 - y1 + (insert_count_x + insert_count_y) * (self.expansion_multiplier - 1)
 
 #content = example.split('\n')
 content = open("input.txt").read().split('\n')
 
-#print_content(content)
 locations = find_galaxy_locations(content)
-#corrected_map = correct_for_cosmic_expansion(content, locations)
 expand_x, expand_y = get_expansion_indices(content, locations)
-#print_content(corrected_map)
-#locations = find_galaxy_locations(corrected_map)
-#print(locations)
 print("Part 1: ", sum(it.starmap(GalaxyDistanceFunctor(2, expand_x, expand_y), it.combinations(locations, 2))))
 print("Part 2: ", sum(it.starmap(GalaxyDistanceFunctor(1000000, expand_x, expand_y), it.combinations(locations, 2))))
