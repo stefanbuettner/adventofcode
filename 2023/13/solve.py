78b688b32ee8327f34a64a0da3958c898d18bf39
Stefan Büttner <buettner.stefan@web.de>
2023-12-13 09:00:55 +0100

    Solution 13.12.2023 - part 2

--- 2023/13/solve.py
+++ 2023/13/solve.py
@@ -16,30 +16,34 @@ example = """#.##..##.
 
 def find_horizontal_mirror(pattern : list):
     for lower_horizontal_mirror_idx in range(len(pattern) - 1):
-        mirror_idx_found = True
         mirror_check_range = min(lower_horizontal_mirror_idx + 1, len(pattern) - lower_horizontal_mirror_idx - 1)
+        num_characters_different = 0
         for y in range(mirror_check_range):
-            if pattern[lower_horizontal_mirror_idx - y] != pattern[lower_horizontal_mirror_idx + y + 1]:
-                mirror_idx_found = False
+            for c in range(len(pattern[lower_horizontal_mirror_idx - y])):
+                if pattern[lower_horizontal_mirror_idx - y][c] != pattern[lower_horizontal_mirror_idx + y + 1][c]:
+                    num_characters_different += 1
+                    if num_characters_different > 1:
+                        break
+            if num_characters_different > 1:
                 break
-        if mirror_idx_found:
+        #print(lower_horizontal_mirror_idx, num_cols_different)
+        if num_characters_different == 1:
             return lower_horizontal_mirror_idx + 1
     return None
 
 def find_vertical_mirror(pattern : list):
     for lower_vertical_mirror_idx in range(len(pattern[0]) - 1):
-        mirror_idx_found = True
         mirror_check_range = min(lower_vertical_mirror_idx + 1, len(pattern[0]) - lower_vertical_mirror_idx - 1)
+        num_characters_different = 0
         for x in range(mirror_check_range):
-            columns_equal = True
             for y in range(len(pattern)):
                 if pattern[y][lower_vertical_mirror_idx - x] != pattern[y][lower_vertical_mirror_idx + x + 1]:
-                    columns_equal = False
-                    break
-            if not columns_equal:
-                mirror_idx_found = False
+                    num_characters_different += 1
+                    if num_characters_different > 1:
+                        break
+            if num_characters_different > 1:
                 break
-        if mirror_idx_found:
+        if num_characters_different == 1:
             return lower_vertical_mirror_idx + 1
     return None
 
@@ -47,5 +51,7 @@ def find_vertical_mirror(pattern : list):
 content = list(map(lambda p: p.split('\n'), open("input.txt").read().split('\n\n')))
 
 vertical_mirrors = filter(None, map(find_vertical_mirror, content))
+#print(vertical_mirrors)
 horizontal_mirrors = filter(None, map(find_horizontal_mirror, content))
-print("Part 1: ", sum(vertical_mirrors) + 100 * sum(horizontal_mirrors))
+#print(horizontal_mirrors)
+print("Part 2: ", sum(vertical_mirrors) + 100 * sum(horizontal_mirrors))
